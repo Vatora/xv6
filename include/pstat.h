bd38911f0d8c4396fa0d3355f88ae862e5978efb
Connor <cbroyles94@gmail.com>
2019-02-18 00:12:17 -0500

    Formatting

--- include/pstat.h
+++ include/pstat.h
@@ -6,13 +6,13 @@
 // Stats for all extant processes
 
 struct pstat {
-  int   inuse[NPROC];      // Whether this slot of the process table is in use
-  int   pid[NPROC];        // PID of each process
-  int   tickets[NPROC];    // Number of tickets this process has
-  int   stride[NPROC];     // Stride of each process calculated from the number of tickets
-  int   pass[NPROC];       // Current pass value of each process
-  int   scheduled[NPROC];  // Number of times each process has been shceduled
-  int   ticks[NPROC];      // Number of ticks each process has accumulated
+  int inuse[NPROC];      // Whether this slot of the process table is in use
+  int pid[NPROC];        // PID of each process
+  int tickets[NPROC];    // Number of tickets this process has
+  int stride[NPROC];     // Stride of each process calculated from the number of tickets
+  int pass[NPROC];       // Current pass value of each process
+  int scheduled[NPROC];  // Number of times each process has been shceduled
+  int ticks[NPROC];      // Number of ticks each process has accumulated
 };
 
 #endif // _PSTAT_H_
